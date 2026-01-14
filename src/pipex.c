c30c36295a41faae4e7ed6b4bc7eb5ee15e7fdcd
Joao Pedro Sequeira Machado <joaseque@c1r12s5.42madrid.com>
2026-01-14 21:21:59 +0100

    main y arreglo de lib

--- src/pipex.c
+++ src/pipex.c
@@ -6,9 +6,21 @@
 /*   By: joaseque <joaseque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 20:26:49 by joaseque          #+#    #+#             */
-/*   Updated: 2026/01/14 20:26:53 by joaseque         ###   ########.fr       */
+/*   Updated: 2026/01/14 21:19:17 by joaseque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+	
+	i = 1;
+	if (argc == 3)
+	{
+		j = 0;
+	}
+	return (0);
+}
